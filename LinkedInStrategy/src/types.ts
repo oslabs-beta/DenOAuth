a20b77feaca6f2688b9ba14cbfff58ba098f7cf1
Daniel <dannguyen1191@yahoo.com>
2021-12-31 18:56:39 -0600

    testing parsing error fix for import Co-authored-by: Matthew Miller <matthewjohnmiller2020@users.noreply.github.com> Co-authored-by: Nechols87 <Nechols87@users.noreply.github.com>

--- LinkedInStrategy/src/types.ts
+++ LinkedInStrategy/src/types.ts
@@ -1,11 +1,11 @@
-// export interface RequestOptions {
-//   /** HTTP Method to use when performing outgoing HTTP requests.  */
-//   method?: HttpVerb;
-//   /** Headers to set when performing outgoing HTTP requests. */
-//   headers?: Record<string, string>;
-//   /** Body parameters to set when performing outgoing HTTP requests. */
-//   body?: Record<string, string>;
-//   /** URL parameters to set when performing outgoing HTTP requests. */
-//   urlParams?: Record<string, string>;
-// }
+export interface RequestOptions {
+  /** HTTP Method to use when performing outgoing HTTP requests.  */
+  // method?: HttpVerb;
+  /** Headers to set when performing outgoing HTTP requests. */
+  headers?: Record<string, string>;
+  /** Body parameters to set when performing outgoing HTTP requests. */
+  body?: Record<string, string>;
+  /** URL parameters to set when performing outgoing HTTP requests. */
+  urlParams?: Record<string, string>;
+}
 
