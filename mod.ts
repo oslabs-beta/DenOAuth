2e1d29a4f453464284447699325c98c1fff5cc5e
Daniel <dannguyen1191@yahoo.com>
2021-12-31 18:42:02 -0600

    testing parsing error fix for import Co-authored-by: Matthew Miller <matthewjohnmiller2020@users.noreply.github.com> Co-authored-by: Nechols87 <Nechols87@users.noreply.github.com>

--- mod.ts
+++ mod.ts
@@ -1,3 +1,3 @@
-// import * from './LinkedInStrategy/src/linkedIn_client.ts'
+// import * from './LinkedInStrategy/src/linkedIn_client.ts' // Can't use * here
 export type { RequestOptions } from './LinkedInStrategy/src/types.ts';
-export { LinkedInClient, LinkedInClientConfig } from './LinkedInStrategy/src/linkedIn_client.ts';
+export { LinkedInClient } from './LinkedInStrategy/src/linkedIn_client.ts';
