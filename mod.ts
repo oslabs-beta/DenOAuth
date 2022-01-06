b5f446a416ae53f01d66a0f594c9deeff869de99
Nechols87 <92054506+Nechols87@users.noreply.github.com>
2022-01-05 19:56:12 -0600

    added mod ts

--- mod.ts
+++ mod.ts
@@ -1,3 +1,4 @@
 export { LinkedInClient } from './LinkedInStrategy/linkedIn_client.ts';
 export { GitHubClient } from './GitHubStrategy/GitHub_client.ts';
 export { GoogleClient } from './GoogleStrategy/Google_client.ts';
+export { SpotifyClient} from './SpotifyStrategy/spotify_client.ts'
