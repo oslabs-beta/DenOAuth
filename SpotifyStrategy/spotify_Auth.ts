dea6a2efabf98e1c298a6fd63eda698a7ec66163
Nechols87 <92054506+Nechols87@users.noreply.github.com>
2022-01-05 20:04:01 -0600

    slice method fix

--- SpotifyStrategy/spotify_Auth.ts
+++ SpotifyStrategy/spotify_Auth.ts
@@ -27,7 +27,7 @@ export class SpotifyStrategy extends SpotifyGrant {
   async processAuth(stringPathName:any) {
    /** Parses the authorization response request tokens from the authorization server. */
     const code:string = JSON.stringify(stringPathName.search);
-    const parsedCode:string = code.slice(code.indexOf('"?code=')+7, code.indexOf('&state'));
+    const parsedCode:string = code.slice(code.indexOf('"?code=')+7);
     const userResponse:unknown[] = [];
     
    /** Exchange the authorization code for an access token */
