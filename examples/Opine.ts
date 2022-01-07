efeaca005d905c8193a47b2af351443873d8200a
Nick Echols <92054506+Nechols87@users.noreply.github.com>
2022-01-07 15:08:51 -0600

    Update Opine.ts

--- examples/Opine.ts
+++ examples/Opine.ts
@@ -1,6 +1,6 @@
 import { opine, serveStatic } from "https://deno.land/x/opine@2.0.2/mod.ts";
 import { renderFileToString } from "https://deno.land/x/dejs@0.10.2/mod.ts";
-import { GitHubClient, LinkedInClient, GoogleClient, SpotifyClient, DiscordClient } from 'https://deno.land/x/denoauth@v1.0.4/mod.ts'
+import { GitHubClient, LinkedInClient, GoogleClient, SpotifyClient, DiscordClient } from 'https://deno.land/x/denoauth@v1.0.6/mod.ts'
 
 
 const app = opine();
